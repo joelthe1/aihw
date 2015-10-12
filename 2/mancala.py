c69a10f0a332955784ff07659a4cefc6f4b764e5
Joel Mathew <joelmathew.mec@gmail.com>
2015-10-12 15:37:41 -0700

    Adding hw2.

---
+++ 2/mancala.py
@@ -0,0 +1,24 @@
+file = 'input.txt' #sys.argv[2]
+with open(file) as inputFile:
+    task = inputFile.readline().strip()
+    my_player = inputFile.readline().strip()
+    depth = inputFile.readline().strip()
+    p2_inp = inputFile.readline().strip().split(' ')
+    p1_inp = inputFile.readline().strip().split(' ')
+    p2_mancala_inp = inputFile.readline().strip()
+    p1_mancala_inp = inputFile.readline().strip()
+
+p1_mancala = len(p1_inp)
+p1_inp.append(p1_mancala_inp)
+p1 = [int(x) for x in p1_inp]
+
+p2_mancala = 0
+p2_inp.append(p2_mancala_inp)
+p2 = [int(x) for x in p2_inp]
+
+state = p1 + p2
+p2_mancala = len(state) - 1
+
+print state
+print p1_mancala
+print p2_mancala
